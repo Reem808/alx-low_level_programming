7ab0f53ff457f9f437652fd555cf73eba9b7f18d
Reem808 <reem25badawy@gmail.com>
2023-02-20 18:14:14 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  char c = 'a';
+
+  while (c <= 'z')
+  {
+    putchar(c);
+    c+=1;
+  }
+  putchar('\n');
+  return (0);
+}
+     
+      
