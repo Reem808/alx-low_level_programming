efc1483b8e55b26cd347bf253b01297ebdbed4c1
Reem808 <reem25badawy@gmail.com>
2023-02-21 18:24:49 -0800

    7 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ * print_alphabet_x10
+ */
+int add(int n1, int n2);
+{
+return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ * print_alphabet_x10
+ */
+int _print_last_digit(int n)
+{
+int l = n % 10;
+return (l);
+}
