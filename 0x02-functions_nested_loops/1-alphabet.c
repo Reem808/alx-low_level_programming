895abc9e23ac82189119bbd99c15e7856851faea
Reem808 <reem25badawy@gmail.com>
2023-02-21 16:37:58 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char c='a';
+
+while (c <= 'z')
+{
+  putchar(c);
+}
+ purchar('\n');
+}
