f571f26bc1d2980f319320a0a09e2b9d992d0835
Reem808 <reem25badawy@gmail.com>
2023-02-21 17:09:48 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+int c = 'a';
+ 
+while( i < 10 )
+{
+c = 'a';
+while( c <= 'z')
+{
+putchar(c);
+c += 1;
+}
+i += 1;
+putchar('\n');
+}
+
+int main(void)
+{
+print_alphabet_x10();
+return (0);
+}
